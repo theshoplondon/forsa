46084455b47e7a25b844e2c3a545975e0f8fd9e6
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-04-01 14:59:31 +0100

    Add "About You" fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_142240) do
+ActiveRecord::Schema.define(version: 2020_04_01_135823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_03_27_142240) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "last_name"
+    t.date "date_of_birth"
   end
 
 end
