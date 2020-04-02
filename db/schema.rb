7a7ad9b84811683a30a636ee144070980430f18a
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-04-02 15:13:11 +0100

    Add contact-details step DB migration
    At present, only Phone number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_090329) do
+ActiveRecord::Schema.define(version: 2020_04_02_100336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_090329) do
     t.string "last_name"
     t.date "date_of_birth"
     t.string "current_step"
+    t.string "phone_number"
   end
 
 end
