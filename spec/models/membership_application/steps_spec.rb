5011766d3573debab7ce142444afa166c96aba2c
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-04-02 15:21:08 +0100

    Add contact-details step
    This is the first step that requires a fleshed-out version of a
    MembershipApplication::Steps class that can tell us if we reached a
    step.
    
    To do this we define a Singleton Steps::PARAMS hash that has the step
    names on the left-hand side and the params names on the right. It works
    out the indices for each and allows us to ask Steps.instance
    .reached_step? for any given step.
    
    While we're about it, we can, at any step, only permit the parameters
    expected at that step in StepsController.

---
+++ spec/models/membership_application/steps_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe MembershipApplication::Steps do
+  subject(:steps) { MembershipApplication::Steps.instance }
+
+  describe '#step_names' do
+    subject { steps.step_names }
+
+    it { is_expected.to include('about-you') }
+  end
+
+  describe '#index_of' do
+    it 'gets the index of a given step' do
+      aggregate_failures do
+        expect(steps.index_of('about-you')).to eql(0)
+        expect(steps.index_of('contact-details')).to eql(1)
+        expect(steps.index_of('non-existent')).to be_nil
+      end
+    end
+  end
+
+  describe '#reached_step?' do
+    subject { steps.reached_step?(step, 'about-you') }
+
+    context 'the application is new' do
+      let(:step) { nil }
+
+      it { is_expected.to be false }
+    end
+
+    context 'the application is at the current step' do
+      let(:step) { 'about-you' }
+
+      it { is_expected.to be true }
+    end
+
+    context 'the application is beyond the current step' do
+      let(:step) { 'contact-details' }
+
+      it { is_expected.to be true }
+    end
+  end
+end
