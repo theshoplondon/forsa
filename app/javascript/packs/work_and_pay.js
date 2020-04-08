8fef50d00d5959939db22975a1cfaf611007bddd
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-04-08 11:18:46 +0100

    Display a monthly estimate for your pay
    Doesn't yet:
    
    - Show a spinner
    - Hold off on asking the API until it has all the details required
    - Have any testing!

--- app/javascript/packs/work_and_pay.js
+++ app/javascript/packs/work_and_pay.js
@@ -1,17 +1,51 @@
 import $ from 'jquery'
+import Rails from '@rails/ujs'
 
-function currentValue() {
-  return $('input[name="membership_application[pay_unit]"]:checked').val();
+function payUnitValue() {
+  return $('input[name="membership_application[pay_unit]"]:checked').val()
 }
 
+const payValue = (name) => $(`input[name="membership_application[${name}]"]`).val()
+
 function setHoursPerWeekVisibility() {
-  $('.hours-per-week').toggleClass('hide', currentValue() !== 'hour')
+  $('.hours-per-week').toggleClass('hide', payUnitValue() !== 'hour')
+}
+
+function getMonthlyEstimate() {
+  fetch(`/subscription-rate?pay_unit=${payUnitValue()}&pay_rate=${payValue('pay_rate')}&hours_per_week=${payValue('hours_per_week')}`, {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json',
+      'X-CSRF-Token': Rails.csrfToken()
+    },
+    credentials: 'same-origin'
+  }).then(function(response) {
+    console.log(response);
+
+    if (response.ok) {
+      return response.json()
+    }
+  }).then(function(data){
+    $('#monthly-estimate-value').text(data['monthly_estimate'])
+    $('.monthly-estimate').removeClass('hide')
+  }).catch((err) => console.log(err))
+}
+
+let timer = null
+
+function estimateInputChanged(interval = 1500) {
+  clearTimeout(timer)
+  timer = setTimeout(getMonthlyEstimate, interval)
 }
 
 $(document).on('turbolinks:load', function() {
   $('input[name="membership_application[pay_unit]"]').change(function () {
     setHoursPerWeekVisibility()
+    estimateInputChanged(1) // Instant when we change unit
   })
+
+  $('input[name="membership_application[pay_rate]"]').on('change keyup', () => estimateInputChanged())
+  $('input[name="membership_application[hours_per_week]"]').on('change keyup', () => estimateInputChanged())
 
   setHoursPerWeekVisibility()
 })
