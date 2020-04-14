6f4f1dea3d64a59bc146bd12ee11500b4424b464
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-04-14 13:27:18 +0100

    Convert steps_controller to FactoryBot
    Would get in the way of home address changes to come next

--- spec/controllers/membership_applications/step_controller_spec.rb
+++ spec/controllers/membership_applications/step_controller_spec.rb
@@ -1,46 +1,33 @@
 require "rails_helper"
 
 RSpec.describe MembershipApplications::StepsController, type: :controller do
-  let(:membership_application) do
-    MembershipApplication.create(
-      current_step: 'contact-details',
-      first_name: 'Natalie',
-      last_name: 'Zurbman',
-      email: 'n@example.com',
-      date_of_birth: '19/04/1973',
-      phone_number: '02345678',
-      job_title: 'Accountant',
-      employer: 'Office of Public Works',
-      work_address: 'Somewhere',
-      payroll_number: '1234',
-      pay_rate: '35000',
-      pay_unit: 'year'
-    )
-  end
+  let(:membership_application) { create :membership_application, :step_work_and_pay }
 
-  before do
-    session[:membership_application_id] = membership_application.id
+  describe 'signing the application' do
+    before do
+      session[:membership_application_id] = membership_application.id
 
-    put :update,
-      params: {
-        id: 'declaration',
-        membership_application: { declaration: declaration }
-      }
+      put :update,
+        params: {
+          id: 'declaration',
+          membership_application: { declaration: declaration }
+        }
 
-    membership_application.reload
-  end
-
-  context 'the declaration is fine' do
-    let(:declaration) { 'Natalie Zurbman' }
-    it 'completes the application by setting current_step to declaration' do
-      expect(membership_application.current_step).to eql('declaration')
+      membership_application.reload
     end
-  end
 
-  context 'the declaration is not correct' do
-    let(:declaration) { 'Notolyu Zarbmen' }
-    it 'leaves the application at the contact-details step' do
-      expect(membership_application.current_step).to eql('contact-details')
+    context 'the declaration is fine' do
+      let(:declaration) { 'Natalie Zurbman' }
+      it 'completes the application by setting current_step to declaration' do
+        expect(membership_application.current_step).to eql('declaration')
+      end
+    end
+
+    context 'the declaration is not correct' do
+      let(:declaration) { 'Notolyu Zarbmen' }
+      it 'leaves the application at the work-and-pay step' do
+        expect(membership_application.current_step).to eql('work-and-pay')
+      end
     end
   end
 end
